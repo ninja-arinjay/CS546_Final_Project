a941a20caefb15ddda558eee38fb632f7dd607b2
Joshua Gorman <90810000+endevii@users.noreply.github.com>
2022-11-29 14:14:11 -0500

    Npm init, and initialize mongo.

--- helpers/userHelper.js
+++ helpers/userHelper.js
@@ -1,60 +1,65 @@
+const { ObjectId } = require("mongodb");
+
 function checkEmail(email) {
-    //email error checking
-    let re = /^[^\s@]+@([^\s@.,]+\.)+[^\s@.,]{2,}$/;
-    if (re.test(email.trim()) === false) {
-      throw "INVALID EMAIL! TRY AGAIN!";
-    }
-};
-  
+  //email error checking
+  let re = /^[^\s@]+@([^\s@.,]+\.)+[^\s@.,]{2,}$/;
+  if (re.test(email.trim()) === false) {
+    throw "INVALID EMAIL! TRY AGAIN!";
+  }
+}
+
 function validatePhone(phone) {
-    //Validate Phone number
-    let re = /^[\+]?[(]?[0-9]{3}[)]?[-\s\.]?[0-9]{3}[-\s\.]?[0-9]{4,6}$/;
-    if (re.test(phone.trim()) === false) {
-      throw "INVALID PHONE NUMBER! TRY AGAIN!";
-    }
-};
+  //Validate Phone number
+  let re = /^[\+]?[(]?[0-9]{3}[)]?[-\s\.]?[0-9]{3}[-\s\.]?[0-9]{4,6}$/;
+  if (re.test(phone.trim()) === false) {
+    throw "INVALID PHONE NUMBER! TRY AGAIN!";
+  }
+}
 function checkInputString(input) {
-    if(!input) throw 'ERROR: ALL FIELDS MUST HAVE AN INPUT!';
-    if (typeof input !== 'string') throw "ERROR: INPUT MUST BE A STRING!";
-    if (input.trim().length === 0)
-      throw 'INPUT CANNOT BE AN EMPTY STRING OR STRING WITH JUST SPACES';
-};
+  if (!input) throw "ERROR: ALL FIELDS MUST HAVE AN INPUT!";
+  if (typeof input !== "string") throw "ERROR: INPUT MUST BE A STRING!";
+  if (input.trim().length === 0)
+    throw "INPUT CANNOT BE AN EMPTY STRING OR STRING WITH JUST SPACES";
+}
 function checkInputPassword(input) {
-    if (!input){
-        throw "ERROR: PASSWORD MUST BE INPUTTED";
-    }
-    if (typeof input !== "string") {
-        throw "ERROR: PASSWORD MUST BE A STRING!"
-    }
-    let rePass = /^[a-zA-Z0-9.\-_$#&@*!]{6,}$/;
-    if (rePass.test(input) === false) {
-        throw "ERROR: Not a valid password!";
-    }
+  if (!input) {
+    throw "ERROR: PASSWORD MUST BE INPUTTED";
+  }
+  if (typeof input !== "string") {
+    throw "ERROR: PASSWORD MUST BE A STRING!";
+  }
+  let rePass = /^[a-zA-Z0-9.\-_$#&@*!]{6,}$/;
+  if (rePass.test(input) === false) {
+    throw "ERROR: Not a valid password!";
+  }
 }
-function checkAge(input){
-    if(!input) throw "Error : Age must have an input."
-    if(typeof input !== 'number') throw "Error : Age must be a number."
-    if(input <16 || input >90) throw "Error : Invalid Age"
+function checkAge(input) {
+  if (!input) throw "Error : Age must have an input.";
+  if (typeof input !== "number") throw "Error : Age must be a number.";
+  if (input < 16 || input > 90) throw "Error : Invalid Age";
 }
-function checkId(input){
-    if (!input) throw "ERROR: ID DOES NOT EXIST";
-    if (typeof input !== "string") {
-        throw "ERROR: ID MUST BE A STRING";
-    }
-    if (input.trim().length === 0) {
-        throw "ERROR: ID CAN'T BE EMPTY STRING";
-    }
+function checkId(input) {
+  if (!input) throw "ERROR: ID DOES NOT EXIST";
+  if (typeof input !== "string") {
+    throw "ERROR: ID MUST BE A STRING";
+  }
+  if (input.trim().length === 0) {
+    throw "ERROR: ID CAN'T BE EMPTY STRING";
+  }
+  if (ObjectId.isValid(id.trim())) {
+    throw "ERROR: NOT A VALID ID - DOESN'T EXIST!";
+  }
 }
 function checkBoolean(input) {
-    if(!input) throw 'ERROR: ALL FIELDS MUST HAVE AN INPUT!';
-    if (typeof input !== 'boolean') throw "ERROR: INPUT MUST BE A BOOLEAN!";
-};
-module.exports ={
-    checkEmail,
-    validatePhone,
-    checkInputString,
-    checkInputPassword,
-    checkAge,
-    checkId,
-    checkBoolean
+  if (!input) throw "ERROR: ALL FIELDS MUST HAVE AN INPUT!";
+  if (typeof input !== "boolean") throw "ERROR: INPUT MUST BE A BOOLEAN!";
+}
+module.exports = {
+  checkEmail,
+  validatePhone,
+  checkInputString,
+  checkInputPassword,
+  checkAge,
+  checkId,
+  checkBoolean,
 };
