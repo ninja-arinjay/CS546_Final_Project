7c45597163c40abf050e501b34862029de5ff87e
Arinjay Panwar <arinjaypanwararp@gmail.com>
2022-12-14 18:18:32 -0500

    Added message for the user to acces the application

--- app.js
+++ app.js
@@ -50,4 +50,7 @@ app.use("/register", (req, res, next) => {
 
 configRoutes(app);
 
-app.listen(3000);
+app.listen(3000, () => {
+  console.log("We've now got a server!");
+  console.log('Your routes will be running on http://localhost:3000');
+});
